9fcaa9cf32e7215b135a8329dc423dec2fa5f320
Yeswanthchandran G <yeswanthchandrang2005@gmail.com>
2025-12-08 11:46:13 +0530

    Add files via upload

---
+++ Device Health Monitoring/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace DeviceMonitor
+{
+    class TemperatureModule
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("kumaran ...");
+            VibrationModule.RunVibrationCheck();
+        }
+    }
+
+    class VibrationModule
+    {
+        public static void RunVibrationCheck()
+        {
+            Console.WriteLine("mathi .");
+        }
+    }
+}
+
