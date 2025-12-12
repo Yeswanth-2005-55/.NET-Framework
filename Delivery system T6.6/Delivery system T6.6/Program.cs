0c75ba82abb0d97e1a05a2cbfda86de1dc6250cc
Yeswanthchandran G <yeswanthchandrang2005@gmail.com>
2025-12-12 12:06:29 +0530

    Add files via upload

---
+++ Delivery system T6.6/Delivery system T6.6/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Notification
+{
+   
+    public virtual void Send(string message)
+    {
+        Console.WriteLine($"Sending notification: {message}");
+    }
+}
+
+
+public class EmailNotification : Notification
+{
+   
+    public override void Send(string message)
+    {
+        Console.WriteLine($"Email sent to registered address: '{message}'");
+    }
+}
+
+
+public class SmsNotification : Notification
+{
+    public override void Send(string message)
+    {
+        Console.WriteLine($"SMS sent to mobile number: '{message}'");
+    }
+}
+
+
+public class PushNotification : Notification
+{
+    public override void Send(string message)
+    {
+        Console.WriteLine($"Push notification delivered to device: '{message}'");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    
+        Notification notificationSender;
+
+        Console.WriteLine("--- Demonstrating Polymorphic Notification System ---");
+
+        notificationSender = new EmailNotification();
+        notificationSender.Send("Your password reset link is here.");
+
+  
+        notificationSender = new SmsNotification();
+        notificationSender.Send("Your 2FA code is 12345.");
+
+
+        notificationSender = new PushNotification();
+        notificationSender.Send("A new message has arrived in your inbox.");
+
+        Console.WriteLine("\n--- Demonstration Complete ---");
+
+    
+        List<Notification> allChannels = new List<Notification>
+        {
+            new EmailNotification(),
+            new SmsNotification(),
+            new PushNotification()
+       
+        };
+
+        Console.WriteLine("\n--- Processing all channels using a single loop (Uniform handling) ---");
+        foreach (var channel in allChannels)
+        {
+
+            channel.Send("Urgent system alert!");
+        }
+    }
+}
